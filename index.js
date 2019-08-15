2b2cad4246b479163f1ca1c4995a75e099c734d5
Dexter Lewis <dexter.lewis@gmail.com>
2019-08-15 15:02:27 -0400

    add helmet and cors

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const winston = require('winston');
+const cors = require('cors')
+const helmet = require('helmet')
+
+server.use(helmet())
+server.use(cors())
 
 require('express-async-errors');
 require('./startup/logging')()
