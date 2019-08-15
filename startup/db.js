e7d9cd7af1077da89ed2af0db4060fe351992f21
Dexter Lewis <dexter.lewis@gmail.com>
2019-08-15 16:10:19 -0400

    Update mongodb connection URL

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const winston = require('winston')
 
-const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost/anonteamfeedback'
+const databaseUrl = process.env.MONGODB_URI || 'mongodb://localhost/anonteamfeedback'
 module.exports = function() {
     mongoose.connect(databaseUrl,  { useNewUrlParser: true })
     .then(() => winston.info('Connected to MongoDB'))
