0b16c398a53c0b79e58384fc6a870ece3f996b13
Dexter Lewis <dexter.lewis@gmail.com>
2019-08-15 01:44:22 -0400

    rearrange imports

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const server = express();
 const winston = require('winston');
 
 require('express-async-errors');
+require('./startup/logging')()
 require('./startup/routes')(server);
 require('./startup/db')();
-require('./startup/logging')()
 
 const port = process.env.PORT || 5050;
 server.listen(port, () => winston.info(`Lisining on port ${port}`));
