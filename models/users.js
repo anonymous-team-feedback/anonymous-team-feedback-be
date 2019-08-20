c2031519f845fc2bd00685742ee293ea30fc1f3a
Dexter Lewis <dexter.lewis@gmail.com>
2019-08-19 23:38:56 -0400

    code formatting

--- models/users.js
+++ models/users.js
@@ -1,45 +1,56 @@
 // Used for Users schema
-const mongoose = require('mongoose')
-const Joi = require('joi')
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
-    name: { 
-        type: String,
-        required: true,
-        minlenght: 2,
-        maxlenght: 50
-    },
-    email: {
-        type: String,
-        required: true,
-        minlenght: 5,
-        maxlenght: 255,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlenght: 5,
-        maxlenght: 1024
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    minlenght: 2,
+    maxlenght: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    minlenght: 5,
+    maxlenght: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlenght: 5,
+    maxlenght: 1024
+  }
+});
 
 // Method for generating the token
 userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({ _id: this._id }, process.env.JWT_PRIVATE_KEY)
-}
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_PRIVATE_KEY);
+};
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
-    const schema = {
-        name: Joi.string().min(2).max(50).required(),
-        email: Joi.string().min(5).max(255).email().required(),
-        password: Joi.string().min(5).max(255).required()
-    }
+  const schema = {
+    name: Joi.string()
+      .min(2)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .email()
+      .required(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+  };
+  return Joi.validate(user, schema);
 }
 
 // Export User model and validation function
-exports.User = User
-exports.validate = validateUser
+exports.User = User;
+exports.validate = validateUser;
