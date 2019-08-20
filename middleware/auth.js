0ab4b58c52bc8d2819c534d2accc8c5c02cb0d16
Dexter Lewis <dexter.lewis@gmail.com>
2019-08-20 02:03:42 -0400

    Updated auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,17 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-module.exports = function (req, res, next) {
-  const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+module.exports = function(req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
-    req.user = decoded; 
+    req.user = decoded;
     next();
+  } catch (ex) {
+    res.status(400).json({ message: "Invalid token." });
   }
-  catch (ex) {
-    res.status(400).send('Invalid token.');
-  }
-}
+};
