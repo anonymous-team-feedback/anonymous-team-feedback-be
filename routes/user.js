dd29858eedcfd2f70381991434a024b23e17b1b2
kaiserawu <kaiser.a.wu@gmail.com>
2019-09-16 20:25:04 -0700

    Add basic update route

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,9 @@ const { User } = require("../models/users");
 const bcrypt = require("bcryptjs");
 
 router.put("/:id", auth, async (req, res) => {
+  const updatedUser = user.findByIdAndUpdate(req.params.id, req.body);
+
+  res.status(201).json(updatedUser);
 })
+
+module.exports = router;
