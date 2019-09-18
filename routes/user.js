4a185956edae64d2bcaa407490da25a0bdbdda52
Christopher Harrison <cnharrison@gmail.com>
2019-09-18 12:17:12 -0400

    remove unused require

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 const user = require("../controllers/user");
-const { User } = require("../models/users");
 const bcrypt = require("bcryptjs");
 
 router.put("/:id", auth, async (req, res) => {
