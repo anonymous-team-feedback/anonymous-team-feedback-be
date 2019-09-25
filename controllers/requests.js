5f295107209a4f671e6a2c573024009c4c1ef5a0
Christopher Harrison <cnharrison@gmail.com>
2019-09-24 21:19:08 -0400

    add request controller

---
+++ controllers/requests.js
@@ -0,0 +1,30 @@
+const { Request } = require("../models/requests.js");
+
+async function findRequestById(requestId) {
+  return await Request.findOne({ _id: requestId });
+}
+
+async function findByRequesterId(requesterId) {
+  return await Post.find({ requester: requesterId }).select("request date");
+}
+
+async function createRequest(requestData) {
+  const request = new Request(requestData);
+  return await request.save();
+}
+
+async function updateRequestById(requestId, updatePairs) {
+  return await Request.findByIdAndUpdate(requestId, updatePairs, { new: true });
+}
+
+async function deleteRequestById(requestId) {
+  return await Post.findOneAndRemove({ requestId });
+}
+
+module.exports = {
+  findRequestById,
+  findByRequesterId,
+  updateRequestById,
+  deleteRequestById,
+  createRequest
+};
