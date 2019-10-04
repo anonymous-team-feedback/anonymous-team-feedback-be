7743fc512b567dc5466babaace7f588b84cfb8b5
Dexter Lewis <dexter.lewis@gmail.com>
2019-10-03 20:03:54 -0400

    Refactor joinTeam functions

--- routes/joinTeam.js
+++ routes/joinTeam.js
@@ -1,26 +1,16 @@
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
-const {
-  requestJoinTeam,
-  getTeamIdBySlug,
-  checkIfManager,
-  updateTeamMembers,
-  getPendingRequest,
-  removeRequest,
-  checkIfTeamMember,
-  checkDuplicateRequest
-} = require("../controllers/joinTeam");
-
+const joinTeam = require("../controllers/joinTeam");
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 
 router.get("/:slug", auth, async (req, res) => {
-  const manager = await checkIfManager(req.user_id);
+  const manager = await joinTeam.checkIfManager(req.user_id);
   if (!manager) {
     res.status(401).json({ message: "You are not the authorizing manager" });
   }
-  const pendingRequest = await getPendingRequest(req.params.slug);
+  const pendingRequest = await joinTeam.getPendingRequest(req.params.slug);
   res.status(200).json(pendingRequest);
 });
 
@@ -30,18 +20,18 @@ router.post("/", auth, async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   // Gets the team id from the provide slug
-  const teamId = await getTeamIdBySlug(req.body.slug);
+  const teamId = await joinTeam.getTeamIdBySlug(req.body.slug);
   if (!teamId) {
     res.status(400).json({ message: "No team found with that slug" });
   }
 
   // Checks if it's a duplicate request
-  const dupRequest = await checkDuplicateRequest(teamId, req.user._id);
+  const dupRequest = await joinTeam.checkDuplicateRequest(teamId, req.user._id);
   if (dupRequest.length > 0) {
     return res.status(400).json({ message: "Duplicate request" });
   }
   //check if user is already part of the team requested to join
-  const team = await checkIfTeamMember(teamId, req.user._id);
+  const team = await joinTeam.checkIfTeamMember(teamId, req.user._id);
   if (team.length > 0) {
     return res
       .status(400)
@@ -49,7 +39,7 @@ router.post("/", auth, async (req, res) => {
   }
 
   // Creates new pending document in database after all checks have past
-  const request = await requestJoinTeam({
+  const request = await joinTeam.requestJoinTeam({
     team: teamId,
     user: req.user._id,
     approved: false
@@ -61,13 +51,13 @@ router.post("/", auth, async (req, res) => {
 router.put("/", auth, async (req, res) => {
   const { error } = validateUpdate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-  const manager = await checkIfManager(req.user_id);
+  const manager = await joinTeam.checkIfManager(req.user_id);
   if (!manager) {
     res.status(401).json({ message: "You are not the authorizing manager" });
   }
 
   if (req.body.approved === "false") {
-    const user = await removeRequest(req.body.user);
+    const user = await joinTeam.removeRequest(req.body.user);
     if (!user) {
       res.status(400).json({ message: "No request was found" });
     }
@@ -76,7 +66,7 @@ router.put("/", auth, async (req, res) => {
       .json({ message: "User has been removed from pending list" });
   }
 
-  const result = await updateTeamMembers(req.body);
+  const result = await joinTeam.updateTeamMembers(req.body);
   res.status(200).json(result[0]);
 });
 
