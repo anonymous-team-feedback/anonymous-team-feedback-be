7066e010b57eb1a7126dbc443fb1a67e7daa8cfc
Alyssa Jane <47366310+xCthaeh@users.noreply.github.com>
2019-10-10 00:03:17 -0400

    fixed typo

--- middleware/error.js
+++ middleware/error.js
@@ -3,5 +3,5 @@ const winston = require("winston");
 module.exports = function(err, req, res, next) {
   winston.error(err.message);
 
-  res.status(500).json({ message: "Inernal server error" });
+  res.status(500).json({ message: "Internal server error" });
 };
